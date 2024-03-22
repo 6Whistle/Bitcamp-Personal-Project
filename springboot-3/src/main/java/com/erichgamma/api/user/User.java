2b3e64c2937901117efe05f1601af90707678c14
6whistle <camjun0506@icloud.com>
2024-03-22 13:20:06 +0900

    UPDATE Entity ADD Order UPDATE user.join

--- springboot-3/src/main/java/com/erichgamma/api/user/User.java
+++ springboot-3/src/main/java/com/erichgamma/api/user/User.java
@@ -1,14 +1,28 @@
 package com.erichgamma.api.user;
 
+import java.util.List;
+
+import com.erichgamma.api.order.Order;
+
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
+@Entity(name = "users")
 @ToString(exclude = "id")
 public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> ordersId;
+
+    private Long addressId;
     private String username;
     private String password;
     private String name;
@@ -16,5 +30,6 @@ public class User {
     private String job;
     private Double height;
     private Double weight;
+
 }
 
